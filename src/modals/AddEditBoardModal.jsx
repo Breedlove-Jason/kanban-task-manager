95b8671339150e263722c37331acaa1d779e549b
Breedlove-Jason <j@jasonbreedlove.com>
2024-06-23 16:07:57 -0700

    add ToDo and Doing inputs on Create Board

--- src/modals/AddEditBoardModal.jsx
+++ src/modals/AddEditBoardModal.jsx
@@ -1,8 +1,21 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function AddEditBoardModal({ setBoardModalOpen, type }) {
   const [name, setName] = useState("");
+  const [newColumns, setNewColumns] = useState([
+    {
+      name: "To Do",
+      tasks: [],
+      id: uuidv4(),
+    },
+    {
+      name: "Doing",
+      tasks: [],
+      id: uuidv4(),
+    },
+  ]);
   return (
     <div
       onClick={(e) => {
@@ -39,6 +52,29 @@ function AddEditBoardModal({ setBoardModalOpen, type }) {
             onChange={(e) => setName(e.target.value)}
             id={"board-name-input"}
           />
+        </div>
+        {/*Board Columns*/}
+        <div className={"mt-8 flex flex-col space-y-3"}>
+          <label className={"text-sm dark:text-white text-gray-500"}>
+            Board Columns
+          </label>
+          {newColumns.map((column, index) => (
+            <div className={"flex items-center w-full"}>
+              <input
+                key={index}
+                className={
+                  "bg-transparent flex-grow px-4 py-2 rounded-md text-sm border border-gray-600 outline-none focus:outline-[#735fc7] "
+                }
+                placeholder={"e.g Web Design"}
+                value={column.name}
+                onChange={(e) => {
+                  const newCols = [...newColumns];
+                  newCols[index].name = e.target.value;
+                  setNewColumns(newCols);
+                }}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
