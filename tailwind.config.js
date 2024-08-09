facd97af34f6cccb4d302c6100612133456e7eb6
Jason Breedlove <j@jasonbreedlove.com>
2024-08-08 18:26:28 -0700

    work on AddModal design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-import tailwindScrollbarHide from "tailwind-scrollbar-hide";
+import tailwindScrollbarHide from 'tailwind-scrollbar-hide';
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
 
   theme: {
     extend: {},
